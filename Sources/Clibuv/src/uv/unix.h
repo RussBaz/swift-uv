890353eabf1cb213843a163b21f02aa0ad361736
RussBaz <RussBaz@users.noreply.github.com>
2024-10-10 23:17:40 +0500

    Modified a header path for a linux build

--- Sources/Clibuv/src/uv/unix.h
+++ Sources/Clibuv/src/uv/unix.h
@@ -46,7 +46,7 @@
 #include "threadpool.h"
 
 #if defined(__linux__)
-# include "uv/linux.h"
+# include "linux.h"
 #elif defined (__MVS__)
 # include "uv/os390.h"
 #elif defined(__PASE__)  /* __PASE__ and _AIX are both defined on IBM i */
